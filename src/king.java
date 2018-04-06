78ce5510394dd9c88c40df9a8e94ae9270cdd8eb
teekingtv <37657628+teekingtv@users.noreply.github.com>
2018-04-06 11:17:38 +0100

    Create king.java

---
+++ src/king.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+public class king {
+
+	public static void main(String[] args) {
+		
+		tee t = new tee();
+		t.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	}
+}
